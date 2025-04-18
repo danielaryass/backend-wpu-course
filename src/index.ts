eb85deebfd058e8933f17b3211b66291f753072f
danielaryass <daniel.aryass7@gmail.com>
2025-04-18 21:32:28 +0700

    Feat: init project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import router from "./routes/api"
+
+const app = express();
+const PORT = 5000 
+
+app.use("/api", router)
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
